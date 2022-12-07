00b515d424b96deb1f8c246d1d552141d9465f16
theranes <120026992+theranes@users.noreply.github.com>
2022-12-07 15:20:15 +0100

    Day 1 solutions - .py

---
+++ Day1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[150]:
+
+
+from pathlib import Path
+txt = Path('Input/day1.txt').read_text()
+list_text=txt.split('\n\n')
+list_text=[item.split('\n') for item in list_text]
+list_text[-1][-1]=0
+list_num=[[int(float(item)) for item in line] for line in list_text]
+list_sums=[sum(line) for line in list_num]
+result=max(list_sums)
+print('The elf carring the most calories is carrying '+str(result)+' calories')
+
+
+# In[ ]:
+
+
+
+
